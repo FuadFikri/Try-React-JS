b1a74a1f5861261b1883ae31391f686bc787f5bc
FuadFikri <fuadfikri01@gmail.com>
2018-10-20 22:47:20 +0700

    components

---
+++ src/Mine.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Mine extends React.Component{
+    render (){
+        return(
+            <div>
+                <div id="mine">
+                    HELLO WORLD
+                </div>
+                < div > React js is cool </div>
+            </div>
+        );
+    }
+}
+
+export default Mine;
